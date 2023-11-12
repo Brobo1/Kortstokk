2c248cdec069c2be2451f0c5682e31776320713c
brobo1 <armanahz95@gmail.com>
2023-11-12 10:37:46 +0100

    reformatted code

--- Kortstokk/Deck.cs
+++ Kortstokk/Deck.cs
@@ -5,51 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Kortstokk {
-    public class Deck {
-        List<PlayingCard> cards { get; } = new();
-        public int Decks{ get; set; }
+	public class Deck {
+		List<PlayingCard> cards { get; set; } = new();
+		public int        Decks { get; set; }
 
 
+		public Deck(int decks = 1) {
+			Decks = decks;
+			CreateDeck();
+		}
 
-        public Deck(int decks = 1) {
-            Decks = decks;
-            CreateDeck();
-        }
-        
-        public void CreateDeck() {
+		public void CreateDeck() {
+			for (int x = 0; x < Decks; x++) {
+				for (int i = 0; i < 4; i++) {
+					for (int j = 0; j < 13; j++) {
+						cards.Add(new(i, j));
+					}
+				}
+			}
 
-            for (int x = 0; x < Decks; x++) {
-                for (int i = 0; i < 4; i++){
-                    for (int j = 0; j < 13; j++) {
-                     
-                        cards.Add(new(i, j));
-                    }       
-                }
-            }
-            foreach (var item in cards) {
-                Console.WriteLine("Suit: " + item.Suit + " " + "Value: " + item.Value);
-            }
-        }
+			foreach (var item in cards) {
+				Console.WriteLine("Suit: " + item.Suit + " " + "Value: " + item.Value);
+			}
+		}
 
-        public void RandDeck(int shuffle) {
-            int count = 0;
-            Random rand = new();
-            for (int i = 0; i < cards.Count()*shuffle; i++) {
-                int random = rand.Next(0, cards.Count());
-                cards.Add(cards[random-1]);
-                cards.RemoveAt(random-1);
-            }
-            foreach (var item in cards) {
-                Console.WriteLine("Suit: " + item.Suit + " Value: " + item.Value);
-                if (item.Suit == 0 || item.Suit == 1) {
-                    count++;
-                }
-            }
-                Console.WriteLine(count + " " + cards.Count());
+		public void RandDeck(int shuffle) {
+			int    count = 0;
+			Random rand  = new();
+			//List <PlayingCard> cardsTemp = cards;
+			//cards = new();
+			Console.WriteLine(cards.Count());
+			for (int i = 0; i <= cards.Count(); i = 1) {
+				int random = rand.Next(0, cards.Count());
+				//cards.Add(cardsTemp[random]);
+				//cardsTemp.RemoveAt(random);
+				cards.Add(cards[random]);
+				cards.RemoveAt(random);
+			}
 
+			foreach (var item in cards) {
+				Console.WriteLine("Suit: " + item.Suit + " Value: " + item.Value);
+				if (item.Suit == 0 || item.Suit == 1) {
+					count++;
+				}
+			}
 
-        }
-
-
-    }
+			Console.WriteLine(count + " " + cards.Count());
+		}
+	}
 }
--- Kortstokk/Program.cs
+++ Kortstokk/Program.cs
@@ -1,13 +1,13 @@
-﻿namespace Kortstokk {
-    internal class Program {
-        static void Main(string[] args) {
-            //Kort kort = new();
+﻿namespace Kortstokk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
             Deck deck = new();
-            Console.WriteLine("\n\n\nShufflelslelslelslesl");
-            deck.RandDeck(1);
-            int counter = 0;
-            List <List <int>> lists = new();
-            
+            Console.WriteLine("\nShufflelslelslelslesl");
+            deck.RandDeck(2);
+
         }
     }
 }
