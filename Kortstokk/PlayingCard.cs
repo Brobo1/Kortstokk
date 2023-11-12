ce809d3405163e73bc82f093d3785f07d51d78b7
brobo1 <armanahz95@gmail.com>
2023-11-12 10:41:16 +0100

    formatt

--- Kortstokk/PlayingCard.cs
+++ Kortstokk/PlayingCard.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kortstokk
-{
-    public class PlayingCard
-    {
-        public int Suit { get; set; }
-        public int Value { get; set; }
-        public bool Visible { get; set; }
+namespace Kortstokk {
+	public class PlayingCard {
+		public int  Suit    { get; set; }
+		public int  Value   { get; set; }
+		public bool Visible { get; set; }
 
-        public PlayingCard(int suit, int value)
-        {
-            Suit = suit;
-            Value = value;
-        }
-    }
+		public PlayingCard(int suit, int value) {
+			Suit  = suit;
+			Value = value;
+		}
+	}
 }
